9d57192ea558c25566149602e1750a26d807e3fe
성현제 <hyunjaesung@seonghyeonje.local>
2020-07-26 00:42:02 +0900

    AppBar 연습

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Practice App',
+    home: MyPage(),
+  ));
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('clicked');
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                print("Notification Clicked");
+              },
+              color: Colors.grey),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("Search Clicked");
+            },
+            color: Colors.grey,
+          ),
+        ],
+        elevation: 6,
+      ),
+    );
+  }
+}
