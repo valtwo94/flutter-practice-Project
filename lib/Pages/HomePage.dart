4063b857f67599622cbf0a4638a4ebe6118eab46
성현제 <hyunjaesung@seonghyeonje.local>
2020-07-26 02:20:38 +0900

    Http 패키지 다운 image Model 생성, api 데이터 가져오기 연습

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:practice1/models/image_model.dart';
+import 'dart:convert';
+
+import 'package:practice1/widgets/image_list.dart';
 
 class HomePage extends StatefulWidget {
   static final String id = 'HomeScreen';
@@ -7,47 +12,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+    print(images);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Colors.black,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('clicked');
-          },
-        ),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: () {
-                print("Notification Clicked");
-              },
-              color: Colors.grey),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print("Search Clicked");
-            },
-            color: Colors.grey,
-          ),
-        ],
-        elevation: 6,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {},
+        appBar: AppBar(
+          title: Text("Home"),
           backgroundColor: Colors.black,
-          label: Text('이미지 추가'),
-          icon: Icon(
-            Icons.add,
-            color: Colors.grey,
-          )),
-      body: Container(
-        color: Colors.black87,
-      ),
-    );
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              print('clicked');
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.notifications),
+                onPressed: () {
+                  print("Notification Clicked");
+                },
+                color: Colors.grey),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print("Search Clicked");
+              },
+              color: Colors.grey,
+            ),
+          ],
+          elevation: 6,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: fetchImage,
+            backgroundColor: Colors.black,
+            label: Text('이미지 추가'),
+            icon: Icon(
+              Icons.add,
+              color: Colors.grey,
+            )),
+        body: ImageList(
+          images: [],
+        ));
   }
 }
---
+++ lib/widgets/image_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:practice1/models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  ImageList({this.images});
+
+  final List<ImageModel> images;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}
