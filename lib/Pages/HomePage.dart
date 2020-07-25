e44c46e9b2fa6e002113bce64ff912f13abbf2e4
성현제 <hyunjaesung@seonghyeonje.local>
2020-07-26 02:56:02 +0900

    ListView 연습

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -23,49 +23,47 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       images.add(imageModel);
     });
-    print(images);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              print('clicked');
-            },
-          ),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.notifications),
-                onPressed: () {
-                  print("Notification Clicked");
-                },
-                color: Colors.grey),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                print("Search Clicked");
-              },
-              color: Colors.grey,
-            ),
-          ],
-          elevation: 6,
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('clicked');
+          },
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: fetchImage,
-            backgroundColor: Colors.black,
-            label: Text('이미지 추가'),
-            icon: Icon(
-              Icons.add,
-              color: Colors.grey,
-            )),
-        body: ImageList(
-          images: [],
-        ));
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                print("Notification Clicked");
+              },
+              color: Colors.grey),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("Search Clicked");
+            },
+            color: Colors.grey,
+          ),
+        ],
+        elevation: 6,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: fetchImage,
+          backgroundColor: Colors.black,
+          label: Text('이미지 추가'),
+          icon: Icon(
+            Icons.add,
+            color: Colors.grey,
+          )),
+      body: Container(color: Colors.black87, child: ImageList(images: images)),
+    );
   }
 }
--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -8,6 +8,38 @@ class ImageList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return Container(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(
+                  images[index].url,
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  width: 300,
+                  child: Text(
+                    images[index].title,
+                    style: TextStyle(color: Colors.white54, fontSize: 30.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                )
+              ],
+            ),
+          ),
+        ));
+      },
+    );
   }
 }
