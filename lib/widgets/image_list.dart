6685e52f15272e603f691791b208de0c7b37262c
성현제 <hyunjaesung@seonghyeonje.local>
2020-07-26 06:49:09 +0900

    Cached Network Image 연습

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:practice1/models/image_model.dart';
 
@@ -12,33 +13,38 @@ class ImageList extends StatelessWidget {
       itemCount: images.length,
       itemBuilder: (context, int index) {
         return Container(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.network(
-                  images[index].url,
-                  width: 200,
-                  height: 200,
-                ),
-                Container(
-                  width: 300,
-                  child: Text(
-                    images[index].title,
-                    style: TextStyle(color: Colors.white54, fontSize: 30.0),
+          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CachedNetworkImage(
+                imageUrl: images[index].url,
+                imageBuilder: (context, imageProvider) => Container(
+                  width: 80.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: imageProvider, fit: BoxFit.cover),
                   ),
                 ),
-                SizedBox(
-                  height: 10.0,
-                )
-              ],
-            ),
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+              Container(
+                width: 300,
+                child: Text(
+                  images[index].title,
+                  style: TextStyle(color: Colors.white54, fontSize: 30.0),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              )
+            ],
           ),
-        ));
+        );
       },
     );
   }
